ac7f3cd567eb45721497a05bc1eee6f092728be9
Hugo Dias <hmdias@gmail.com>
2024-08-23 21:14:56 +0100

    Add new scenes

---
+++ Scenes/Scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const SPEED = 3000.0
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction:
+		velocity = direction * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+		
+	move_and_slide()
